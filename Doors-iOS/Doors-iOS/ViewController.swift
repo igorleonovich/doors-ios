fd807407d518b3a4dc3fb01d38ee44bc08654a19
Igor Leonovich <email@domain.com>
2021-06-23 15:57:14 +0300

    DRS-10: Project init

---
+++ Doors-iOS/Doors-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Doors-iOS
+//
+//  Created by Igor Leonovich on 23.06.21
+//  Copyright © 2021 FT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
